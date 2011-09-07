d9af1beafc84e7692bb787f5261b3d5b8f0f0711
Simon Knight <simon.knight@gmail.com>
2011-09-07 23:50:27 +0930

    Fix exclusion of __init__.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup (
      name = "topzootools",
-     version = "0.3.14",
+     version = "0.3.15",
      description = "Processing and conversion tools for Internet Topology Zoo",
      long_description="Processing and conversion tools for Internet Topology Zoo",
                
